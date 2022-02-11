6d98b0e553662e52ed94d132ad552fb30ff46859
vanely <vanelys94@gmail.com>
2022-02-11 00:26:50 -0500

    allowing base directories update

--- env-creation/directories.sh
+++ env-creation/directories.sh
@@ -10,14 +10,35 @@ CURRENT_WORKING_TREE=""
 update_dir_tree() {
   # import config file
   source "${1}"
-  # echo "Current config from directories.sh: ${1}"
+
   if [[ -f "${1}" ]] ; then
+
+    # check for and add new base directories
+    if [[ -n "${BASE_DIR_ARRAY}" ]] ; then
+      BASE_DIR_ARR_LEN="${#BASE_DIR_ARRAY[@]}"
+      for (( i=0; i<"${BASE_DIR_ARR_LEN}"; i++ ))
+      do
+        if [[ ! -d "${BASE_DIR_ARRAY[i]}" ]] ; then
+          echo
+          echo "========================================================================================="
+          echo "Adding base directory..."
+          echo "${BASE_DIR_ARRAY[i]}"
+          # -p flag allows for the crewation of nested dirs
+          mkdir -p "${BASE_DIR_ARRAY[i]}"
+        fi
+      done
+    else
+      echo "It seems your BASE_DIR_ARRAY is empty"
+    fi
+
+    # check for and add new project directories, and clone their corresponding projects
     if [[ -n "${DIR_ARRAY}" ]] && [[ -n "${CORRESPONDING_PROJECTS_ARRAY}" ]] ; then
       DIR_ARR_LEN="${#DIR_ARRAY[@]}"
       # will have to consume from config file
       for (( i=0; i<"${DIR_ARR_LEN}"; i++ ))
       do
-        if [[ -n "$(ls ${DIR_ARRAY[i]} 2> /dev/null)" ]] ; then
+        # if [[ -n "$(ls ${DIR_ARRAY[i]} 2> /dev/null)" ]] ; then
+        if [[ -d "${DIR_ARRAY[i]}" ]] ; then
           echo "========================================================================================="
           echo "No update needed for: ${DIR_ARRAY[i]}"
           echo "========================================================================================="
